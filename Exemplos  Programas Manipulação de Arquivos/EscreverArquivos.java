ab63c51341098bc3045f1ae5b5a2b62a261547fa
Murilo de Sousa <murilodesousag@gmail.com>
2022-08-17 23:08:39 -0300

    leitura de arquivo feita. Falta validar admin

--- Exemplos  Programas Manipulação de Arquivos/EscreverArquivos.java
+++ Exemplos  Programas Manipulação de Arquivos/EscreverArquivos.java
@@ -6,12 +6,14 @@ public class EscreverArquivos {
     
     public static void main(String[] args){
         List<String> lista = new ArrayList<String>();
-        lista.add("Humberto;24 anos;santoandre");
-        lista.add("Murilo;27 anos;saopaulo");
+        lista.add("ID,nome,senha,email,data de criação, criado por, flagAdmin");
+        lista.add("1,user,senha,user@ufabc.com.br");
+        lista.add("2,humberto,admin,humberto@ufabc.com.br");
+        lista.add("3,murilo,admin,murilo@ufabc.com.br,");
         try{
-        FileWriter fw = new FileWriter(new File("/Users/humberto.t.marinho/OneDrive - Accenture/Desktop/ler arquivos no java/teste.csv"));
+        FileWriter fw = new FileWriter(new File("/Users/murilosousa/Library/CloudStorage/GoogleDrive-murilodesousag@gmail.com/Meu Drive/_UFABC/_2022.2/POO - Programação Orientada a Objeto/PROJETO/POO-ufabc/sistemaVotacao/DataBase/User.csv"));
         PrintWriter pw = new PrintWriter(fw);
-        for (int i = 0; i<2; i++){
+        for (int i = 0; i<4; i++){
         String linha= lista.get(i);
         pw.println(linha);
         }
--- Exemplos  Programas Manipulação de Arquivos/LerLinhas.java
+++ Exemplos  Programas Manipulação de Arquivos/LerLinhas.java
@@ -2,42 +2,38 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.lang.NumberFormatException;
- 
-class LerLinhas {
- 
-    public static void main(String data[])
-    {
- 
-        try {
- 
-            String nameNumberString;
- 
-            // Using file pointer creating the file.
-            File file = new File("/Users/humberto.t.marinho/OneDrive - Accenture/Desktop/ler arquivos no java/contatos.txt");
 
- 
+class LerLinhas {
+
+    public static void main(String data[]) {
+
+        try {
+            String nameNumberString;
+
+            // Using file pointer creating the file.
+            File file = new File(
+                    "/Users/murilosousa/Library/CloudStorage/GoogleDrive-murilodesousag@gmail.com/Meu Drive/_UFABC/_2022.2/POO - Programação Orientada a Objeto/PROJETO/POO-ufabc/sistemaVotacao/DataBase/User.csv");
+
             // Opening file in reading and write mode.
- 
+
             RandomAccessFile raf = new RandomAccessFile(file, "rw");
- 
+
             // Traversing the file
             // getFilePointer() give the current offset
             // value from start of the file.
             while (raf.getFilePointer() < raf.length()) {
- 
+
                 // reading line from the file.
                 nameNumberString = raf.readLine();
 
                 // utilo String[] lineSplit = nameNumberString.split("!");
- 
+
                 // Print the contact data
                 System.out.println(nameNumberString);
             }
+        } catch (IOException ioe) {
+
+            System.out.println(ioe);
         }
-            catch (IOException ioe)
-            {
- 
-                System.out.println(ioe);
-            }
     }
 }
